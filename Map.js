b7f3f546eaa0da4e4b65ef82b99e9263c2826b74
Nehal <nehal.soni@gmail.com>
2016-04-04 11:00:14 +0100

    style so more screen space, also map is async loaded, so need intialise map once window is fully loaded

--- Map.js
+++ Map.js
@@ -1,126 +1,130 @@
-var globalMap;
-
 $(function() {
 
-    var currAirport = {};
-    var marker = {};
-    var currMarkerId;
+	var globalMap;
 
-    var MapFcns = {
+		var currAirport = {};
+		var marker = {};
+		var currMarkerId;
 
-        loadSiteList: function() {
-            var airportList = $('#airport-list');
-            airportList.html('');
-            airportList.append('<option value=""></option>');
+		var MapFcns = {
 
-            //sort by airport codes
-            var sortedSites = [];
-            sortedSites = _.sortBy(sites, function(a) {
-                return a.Code;
-            });
+				loadSiteList: function() {
+						var airportList = $('#airport-list');
+						airportList.html('');
+						airportList.append('<option value=""></option>');
 
-            for (var i in sortedSites) {
-                var newOption = $('<option value="' + sortedSites[i].Code + '">' + sortedSites[i].Code + '</option>');
-                airportList.append(newOption);
-            }
-        },
+						//sort by airport codes
+						var sortedSites = [];
+						sortedSites = _.sortBy(sites, function(a) {
+								return a.Code;
+						});
 
-        clear: function() {
-            currAirport = {};
-            $('#remove-airport').hide();
-            $('#airport-list').prop('selectedIndex', -1); //reset select box
+						for (var i in sortedSites) {
+								var newOption = $('<option value="' + sortedSites[i].Code + '">' + sortedSites[i].Code + '</option>');
+								airportList.append(newOption);
+						}
 
-            $('#setting-code').text('');
-            $('#setting-city').text('');
-            $('#setting-state').text('');
-            $('#setting-name').text('');
-            $('#setting-lat').text('');
-            $('#setting-long').text('');
-        },
+				},
 
-        siteListChange: function() {
-            var ctl = $(this);
-            airportCode = ctl.val();
+				clear: function() {
+						currAirport = {};
+						$('#remove-airport').hide();
+						$('#airport-list').prop('selectedIndex', -1); //reset select box
 
-            if (airportCode) {
+						$('#setting-code').text('');
+						$('#setting-city').text('');
+						$('#setting-state').text('');
+						$('#setting-name').text('');
+						$('#setting-lat').text('');
+						$('#setting-long').text('');
+				},
 
+				siteListChange: function() {
+						var ctl = $(this);
+						airportCode = ctl.val();
 
-                //if airport same as current selection then exit
-                if (currAirport.Code === airportCode)
-                    return;
-
-                currAirport = _.findWhere(sites, {
-                    Code: airportCode
-                });
-
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                $('#setting-state').text(currAirport.State);
-                $('#setting-name').text(currAirport.FullSiteName);
-                $('#setting-lat').text(currAirport.Latitude);
-                $('#setting-long').text(currAirport.Longitude);
-
-                var point = {
-                    lat: currAirport.Latitude,
-                    lng: currAirport.Longitude
-                };
-
-                marker = new google.maps.Marker({
-                    id: currAirport.Code,
-                    position: point,
-                    map: globalMap,
-                    title: currAirport.Code
-                });
-
-                //move to the marker position
-                globalMap.panTo(point);
-
-                $('#remove-airport').show(); //since selected airport, give option to remove
-            }
-        },
-
-        removeSite: function() {
-            if (currAirport) {
-                marker.setMap(null);
-                this.clear();
-            }
-            return false;
-        }
-
-    }
+						if (airportCode) {
 
 
-    MapFcns.loadSiteList();
-    $('#airport-list').change(MapFcns.siteListChange);
-    $('#remove-airport').hide(); //hide remove button initially, until selection is made
-    $('#remove-airport').click(function(evt) {
-        evt.preventDefault(); //stop anchor from performing default behaviour
-        MapFcns.removeSite();
-    });
-    $('#exercise-toggle').click(function() {
-        var toggleCtl = $(this),
-            toggleVal = toggleCtl.text();
-        if (toggleVal == '-') {
-            toggleCtl.text('+');
-            $('#exercise-instructions').hide();
-        } else {
-            toggleCtl.text('-');
-            $('#exercise-instructions').show();
-        }
-    });
+								//if airport same as current selection then exit
+								if (currAirport.Code === airportCode)
+										return;
 
-});
+								currAirport = _.findWhere(sites, {
+										Code: airportCode
+								});
+
+								$('#setting-code').text(currAirport.Code);
+								$('#setting-city').text(currAirport.City);
+								$('#setting-state').text(currAirport.State);
+								$('#setting-name').text(currAirport.FullSiteName);
+								$('#setting-lat').text(currAirport.Latitude);
+								$('#setting-long').text(currAirport.Longitude);
+
+								var point = {
+										lat: currAirport.Latitude,
+										lng: currAirport.Longitude
+								};
+
+								marker = new google.maps.Marker({
+										id: currAirport.Code,
+										position: point,
+										map: globalMap,
+										title: currAirport.Code
+								});
+
+								//move to the marker position
+								globalMap.panTo(point);
+
+								$('#remove-airport').show(); //since selected airport, give option to remove
+						}
+				},
+
+				removeSite: function() {
+						if (currAirport) {
+								marker.setMap(null);
+								this.clear();
+						}
+						return false;
+				}
+
+		}
+
+
+		MapFcns.loadSiteList();
+		$('#airport-list').change(MapFcns.siteListChange);
+		$('#remove-airport').hide(); //hide remove button initially, until selection is made
+		$('#remove-airport').click(function(evt) {
+				evt.preventDefault(); //stop anchor from performing default behaviour
+				MapFcns.removeSite();
+		});
+		$('#exercise-toggle').click(function() {
+				var toggleCtl = $(this),
+						toggleVal = toggleCtl.text();
+				if (toggleVal == '-') {
+						toggleCtl.text('+');
+						$('#exercise-instructions').hide();
+				} else {
+						toggleCtl.text('-');
+						$('#exercise-instructions').show();
+				}
+		});
+
+
 
 
 function initMap() {
-    // Callback function to create a map object and specify the DOM element for display.
-    globalMap = new google.maps.Map(document.getElementById('airport-map'), {
-        center: {
-            lat: 42.2814,
-            lng: -83.7483
-        },
-        scrollwheel: true,
-        zoom: 6
-    });
-
+		// Callback function to create a map object and specify the DOM element for display.
+		globalMap = new google.maps.Map(document.getElementById('airport-map'), {
+				center: {
+						lat: 42.2814,
+						lng: -83.7483
+				},
+				scrollwheel: true,
+				zoom: 6
+		});
 }
+
+//once the window has loaded, initialise the map
+window.onload = initMap;
+});
